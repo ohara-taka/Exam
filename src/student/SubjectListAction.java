e2fbaa117aceb9f2f2fb8b743406bb1a0ebb528c
yanoren <rentatensai1974@gmail.com>
2024-06-12 12:04:44 +0900

    SubjectDAOを作成、SubjectListActionを更新しました。

--- src/student/SubjectListAction.java
+++ src/student/SubjectListAction.java
@@ -1,14 +1,22 @@
 package student;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import bean.Subject;
+import dao.SubjectDAO;
 import tool.Action;
 
 public class SubjectListAction extends Action {
 	public String execute(
 		HttpServletRequest request, HttpServletResponse response
 	) throws Exception {
+		SubjectDAO dao=new SubjectDAO();
+		List<Subject> subjectList=dao.subjectAll();
+
+		request.setAttribute("subjectList", subjectList);
 
 
 		// FrontControllerを使用しているためreturn文でフォワードできる
