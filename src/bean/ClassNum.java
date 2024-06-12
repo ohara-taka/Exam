d660424948a1da21943083f382143799b30c56fd
watanabtakuma <ooi2372019@stu.o-hara.ac.jp>
2024-06-12 11:15:05 +0900

    ClassNumbean作成

---
+++ src/bean/ClassNum.java
@@ -0,0 +1,27 @@
+package bean;
+
+// Serializableインターフェースを実装してBeanを作成する
+public class ClassNum implements java.io.Serializable {
+
+    // DBの項目名と同じのprivateなフィールドを定義
+    private School school;
+    private String num; // コースIDを追加
+
+
+    // ゲッターメソッド
+    public School getSchool() {
+        return school;
+    }
+    public String getNum() {
+        return num;
+    }
+
+
+    // セッターメソッド
+    public void setSchool(School school) {
+        this.school = school;
+    }
+    public void setNum(String num) {
+        this.num = num;
+    }
+}
