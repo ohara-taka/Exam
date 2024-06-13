a6cbd1717d715602c51d780f55c3ca50b37295d9
ohara-taka <ooi2372409@stu.o-hara.ac.jp>
2024-06-13 11:00:20 +0900

    ログイン用ページとcssの調整

--- WebContent/student/login.jsp
+++ WebContent/student/login.jsp
@@ -1,8 +1,8 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
     <%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
-    <!-- CSS ファイルのリンク -->
-<link rel="stylesheet" type="text/css" href="../css/style.css">
+    <!-- CSS ファイルのリンク "${pageContext.request.contextPath}/css/style.css" -->
+<link rel="stylesheet" type="text/css" href="${pageContext.request.contextPath}/css/style.css">
 <%@include file="../header.jsp" %>
 <div class="container">
 
--- src/student/LoginAction.java
+++ src/student/LoginAction.java
@@ -1,17 +1,28 @@
 package student;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import tool.Action;
 
-public class LoginAction extends Action {
-	public String execute(
+@WebServlet(urlPatterns={"/"})
+public class LoginAction extends HttpServlet {
+	public void doGet (
 		HttpServletRequest request, HttpServletResponse response
-	) throws Exception {
-
-
-		// FrontControllerを使用しているためreturn文でフォワードできる
-		return "login.jsp";
+	) throws ServletException, IOException {
+		PrintWriter out=response.getWriter();
+		try {
+			// index.jspへフォワードするだけ
+			request.getRequestDispatcher("login.jsp")
+				.forward(request, response);
+		} catch (Exception e) {
+			e.printStackTrace(out);
+		}
 	}
 }
+
