cf2ab727166adfa6751dae6e726635c16790d55b
hazuku10 <ooi2372411@stu.o-hara.ac.jp>
2024-06-19 10:05:54 +0900

    StudentCreateAction・StudentCreateExeciteActionを修正

--- src/student/StudentCreateAction.java
+++ src/student/StudentCreateAction.java
@@ -5,10 +5,25 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import bean.ClassNum;
+import bean.Teacher;
 import dao.ClassNumDao;
-import tool.Action; // Actionクラスのインポート
+import dao.TeacherDao;
+import tool.Action;
+
 
 public class StudentCreateAction extends Action {
+<<<<<<< Updated upstream
     @Override
     public String execute(
             HttpServletRequest request, HttpServletResponse response
@@ -16,13 +31,19 @@ public class StudentCreateAction extends Action {
         // ClassNumDaoを使用してクラス番号のリストを取得
         ClassNumDao classNumDao = new ClassNumDao();
         List<String> classNumList = classNumDao.filter(); // List<String> に変更
+=======
+>>>>>>> Stashed changes
 
-        // リクエストにクラス番号のリストをセット
-        request.setAttribute("classNumList", classNumList);
+	@Override
+	public String execute(
+		HttpServletRequest request, HttpServletResponse response
+	) throws Exception {
+		HttpSession session = request.getSession(); // セッション
+		Teacher teacher = (Teacher)session.getAttribute("user");
 
-        // JSPページにフォワードする
-        request.getRequestDispatcher("student_create.jsp").forward(request, response);
 
-        return null; // この行はvoidのように見えるが、実際のActionフレームワークに依存する
-    }
+
+		// FrontControllerを使用しているためreturn文でフォワードできる
+		return "student_create.jsp";
+	}
 }
