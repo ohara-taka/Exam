848d0e88a5ece4fda1f003a3887230953b727b7f
hazuku10 <ooi2372411@stu.o-hara.ac.jp>
2024-06-19 13:54:45 +0900

    StudentCreateAction修正

--- src/student/StudentCreateAction.java
+++ src/student/StudentCreateAction.java
@@ -5,45 +5,33 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-import bean.ClassNum;
 import bean.Teacher;
 import dao.ClassNumDao;
-import dao.TeacherDao;
 import tool.Action;
-
+import util.Util;
 
 public class StudentCreateAction extends Action {
-<<<<<<< Updated upstream
     @Override
     public String execute(
-            HttpServletRequest request, HttpServletResponse response
+        HttpServletRequest request, HttpServletResponse response
     ) throws Exception {
+
+        // セッションからTeacherオブジェクトを取得
+
+    	Teacher teacher = Util.getUser(request);
+
+//        HttpSession session = request.getSession();
+//        Teacher teacher = (Teacher) session.getAttribute("user");
+
         // ClassNumDaoを使用してクラス番号のリストを取得
         ClassNumDao classNumDao = new ClassNumDao();
-        List<String> classNumList = classNumDao.filter(); // List<String> に変更
-=======
->>>>>>> Stashed changes
+        List<String> classNumList = classNumDao.filter(teacher.getSchool()); // Schoolオブジェクトを渡さない
 
-	@Override
-	public String execute(
-		HttpServletRequest request, HttpServletResponse response
-	) throws Exception {
-		HttpSession session = request.getSession(); // セッション
-		Teacher teacher = (Teacher)session.getAttribute("user");
+        // リクエストにクラス番号のリストをセット
+        request.setAttribute("classNumList", classNumList);
 
-
-
-		// FrontControllerを使用しているためreturn文でフォワードできる
-		return "student_create.jsp";
-	}
+        // JSPページにフォワードする
+        return "student_create.jsp";
+    }
 }
+
